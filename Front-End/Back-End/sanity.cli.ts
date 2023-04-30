c94a0af10397388867620ab7656ef4cede8200c4
Muhammad Mubashir Munir Khan <100374421+mubashir05-beep@users.noreply.github.com>
2023-04-30 05:03:07 -0700

    added sanity schemas and in next app

---
+++ Front-End/Back-End/sanity.cli.ts
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'tk5cfny0',
+    dataset: 'production'
+  }
+})
---
+++ Front-End/khabi_store/pages/index.js
@@ -0,0 +1,14 @@
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <main
+      className={`flex bg-black`}
+    >
+     hello
+    </main>
+  )
+}
