8e96dedac5526b71458c73950708b55d0fcf678b
Muhammad Mubashir Munir Khan <100374421+mubashir05-beep@users.noreply.github.com>
2023-05-01 23:20:16 -0700

    connected the banner to sanity

--- Front-End/khabi_store/pages/index.js
+++ Front-End/khabi_store/pages/index.js
@@ -1,20 +1,27 @@
 import HeroBanner from "@/components/HeroBanner";
+import Products from "@/components/Products";
 import React from "react";
+import HeaderTitle from "@/components/HeaderTitle";
 import { client } from "@/lib/client";
-export default function Home({products,bannerData}) {
-  return <main >
-    <HeroBanner setBannerData={bannerData.length&&bannerData[0]}/>
-  </main>;
+export default function Home({ products, bannerData }) {
+  return (
+    <main>
+      <HeroBanner setBannerData={bannerData} />
+      <HeaderTitle />
+      {}
+    </main>
+  );
 }
 
-export const getServerSideProps=async()=>{
-    const query ='*[_type=="product"]';
-    const products =await client.fetch(query);
-    const bannerQuery ='*[_type=="banner"]';
-    const bannerData =await client.fetch(bannerQuery);
-    return{
-      props:{
-        products,bannerData
-      }
-    }
-}
+export const getServerSideProps = async () => {
+  const query = '*[_type=="product"]';
+  const products = await client.fetch(query);
+  const bannerQuery = '*[_type=="banner"]';
+  const bannerData = await client.fetch(bannerQuery);
+  return {
+    props: {
+      products,
+      bannerData,
+    },
+  };
+};
