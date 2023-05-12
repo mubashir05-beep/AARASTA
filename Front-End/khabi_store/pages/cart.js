bc6ebb35376c159fcfe0ef25bf47317e990d8252
Muhammad Mubashir Munir Khan <100374421+mubashir05-beep@users.noreply.github.com>
2023-05-12 13:36:13 -0700

    updates

--- Front-End/khabi_store/pages/cart.js
+++ Front-End/khabi_store/pages/cart.js
@@ -46,77 +46,79 @@ const Cart = () => {
         >
           {cartItems.length >= 1 &&
             cartItems.map((items, index) => (
-              <div className="flex border-b  py-5 " key={items._id}>
-                <div className="flex gap-[3rem] justify-evenly w-[100%] ">
+              <div className="flex  border-b  py-5 " key={items._id}>
+                <div className="flex min-[500px]:flex-row md:flex-row items-center md:items-start flex-col gap-6 md:gap-[3rem] md:justify-evenly w-[100%] ">
                   <img
                     src={urlFor(items?.image[0])}
                     width={"200px"}
                     className="rounded-lg"
                   />
-                  <div className="flex flex-col gap-2 my-6 w-[200px]">
-                    <div className="font-semibold text-lg break-words">
-                      {items.name}
-                    </div>
-                    <div className="text-[15px] text-gray-500">
-                      Code: {items.productCode}
-                    </div>
-                    <div className="text-[15px] text-gray-500">
-                      {items.category}
-                    </div>
-                    <div className="text-[15px] flex gap-1 items-center py-1 text-gray-500">
-                      {" "}
-                      Size:
-                      <span className="w-[20px] h-[20px] text-[12px] text-black rounded-full bg-black/[0.1] flex items-center justify-center">
-                        {size}
-                      </span>
-                    </div>
-                    <div>
-                      {items.quantity ? (
-                        <div className="text-green-400 text-[14px]">
-                          In Stock
-                        </div>
-                      ) : (
-                        <div className="text-red-400 text-[14px]">
-                          Out of Stock
-                        </div>
-                      )}
-                    </div>
-                  </div>
-                  <div className="flex flex-col gap-2 w-[100px] items-center my-6">
-                    <div className="flex flex-col gap-2 my-1  items-center">
-                      <div className="font-[500] text-[15px] text-lg">
-                        Per Price
+                  <div className="flex flex-col md:flex-row items-center min-[500px]:items-start">
+                    <div className="flex flex-col gap-2 items-center  min-[500px]:items-start md:my-6 w-[200px]">
+                      <div className="font-semibold text-lg break-words">
+                        {items.name}
                       </div>
-                      <div className="text-gray-600">PKR {items.price}/-</div>
-                    </div>
-                    <div className="flex flex-col my-1 gap-2  items-center ">
-                      <div className="font-[500] text-[15px] text-lg">
-                        Quantity
+                      <div className="text-[15px] text-gray-500">
+                        Code: {items.productCode}
                       </div>
-                      <div className="flex items-center ">
-                        <button
-                          className="px-2 bg-gray-200 text-gray-700 rounded-l"
-                          onClick={() =>
-                            toggleCartItemQuanitity(items._id, "dec")
-                          }
-                        >
-                          -
-                        </button>
-                        <span className="mx-2 text-lg text-gray-600">
-                          {items.quantity}
+                      <div className="text-[15px] text-gray-500">
+                        {items.category}
+                      </div>
+                      <div className="text-[15px] flex gap-1 items-center py-1 text-gray-500">
+                        {" "}
+                        Size:
+                        <span className="w-[20px] h-[20px] text-[12px] text-black rounded-full bg-black/[0.1] flex items-center justify-center">
+                          {size}
                         </span>
-                        <button
-                          className="px-2  bg-gray-200 text-gray-700 rounded-r"
-                          onClick={() =>
-                            toggleCartItemQuanitity(items._id, "inc")
-                          }
-                        >
-                          +
-                        </button>
+                      </div>
+                      <div className="hidden md:block">
+                        {items.quantity ? (
+                          <div className="text-green-400 text-[14px]">
+                            In Stock
+                          </div>
+                        ) : (
+                          <div className="text-red-400 text-[14px]">
+                            Out of Stock
+                          </div>
+                        )}
+                      </div>
+                    </div>
+                    <div className="flex flex-col  gap-2 md:w-[100px] md:items-center md:my-6">
+                      <div className="flex flex-col gap-2 md:my-1  items-center">
+                        <div className="font-[500] text-[15px] text-lg hidden md:block">
+                          Per Price
+                        </div>
+                        <div className="text-gray-600">PKR {items.price}/-</div>
+                      </div>
+                      <div className="flex flex-col md:my-1 gap-2  items-center ">
+                        <div className="font-[500] text-[15px] text-lg hidden md:block">
+                          Quantity
+                        </div>
+                        <div className="flex items-center ">
+                          <button
+                            className="px-2 bg-gray-200 text-gray-700 rounded-l"
+                            onClick={() =>
+                              toggleCartItemQuanitity(items._id, "dec")
+                            }
+                          >
+                            -
+                          </button>
+                          <span className="mx-2 text-lg text-gray-600">
+                            {items.quantity}
+                          </span>
+                          <button
+                            className="px-2  bg-gray-200 text-gray-700 rounded-r"
+                            onClick={() =>
+                              toggleCartItemQuanitity(items._id, "inc")
+                            }
+                          >
+                            +
+                          </button>
+                        </div>
                       </div>
                     </div>
                   </div>
-                  <div className="w-[100px] flex justify-center">
+                  <div className="w-[100px] md:block hidden">
                     <RxCross2
                       size={25}
                       className="cursor-pointer"
@@ -127,7 +129,7 @@ const Cart = () => {
               </div>
             ))}
         </div>
-        <div className="flex flex-[0.5] border-l border-r border-b">
+        <div className="flex flex-[0.5] border-l border-r justify-center border-b">
           {cartItems.length >= 1 && (
             <div className=" px-5 py-5 flex flex-col items-center justify-around  h-[556px] ">
               <div className="flex flex-col min-[1534px]:flex-row items-center bg-red-200 p-3 w-[100%] text-black gap-5">
@@ -157,7 +159,10 @@ const Cart = () => {
 
                   <div className="flex items-center gap-2 w-[100%] bg-gray-600 text-white px-1">
                     <p className="font-medium text-[16px]  ">Grand Total:</p>
-                    <div className=" text-[16px]">PKR {totalPrice + 99}/- <span className="text-sm"> (incl shipping fee)</span></div>
+                    <div className=" text-[16px]">
+                      PKR {totalPrice + 99}/-{" "}
+                      <span className="text-sm"> (incl shipping fee)</span>
+                    </div>
                   </div>
                 </div>
               </div>
