df6bcf008b4ef5e6693bbecff908964c49d4b51a
Muhammad Mubashir Munir Khan <100374421+mubashir05-beep@users.noreply.github.com>
2023-06-06 10:44:56 -0700

    Update success.js

--- Front-End/khabi_store/pages/success.js
+++ Front-End/khabi_store/pages/success.js
@@ -1,8 +1,6 @@
 import React, { useEffect } from "react";
 import { useRouter } from "next/router";
 import { useStateContext } from "@/context/StateContext";
-import Link from "next/link";
-
 const Success = () => {
   const router = useRouter();
   const { deleteCart } = useStateContext();
@@ -14,11 +12,40 @@ const Success = () => {
   const gotoPage = () => {
     router.push("/ready_to_wear");
   };
+  const gotoHome = () => {
+    router.push("/");
+  };
 
   return (
-    <div className="flex flex-col justify-center items-center  h-[80vh]">
-      <h1 className='font-semibold text-4xl'>Thank you for your order</h1>
-      <div onClick={gotoPage} className="cursor-pointer py-2 px-3 rounded-xl background- border my-[14px]">Go Home</div>
+    <div className="flex flex-col justify-center mx-[2rem] items-center h-[80vh]">
+      <h1 className="font-semibold text-4xl max-[470px]:text-3xl text-center">
+        Your order is Complete!
+      </h1>
+      <h1 className="font-medium my-[12px] text-xl max-[470px]:text-lg ">
+        Thank you for choosing us!
+      </h1>
+      <p className="max-w-[600px] text-center word-break">
+        Your order was successfully received, and an email confirmation has been
+        sent to your registered email address. Please check your inbox, and if
+        you don't find the email, kindly check your spam or junk folder. If you
+        need any further assistance, please don't hesitate to contact our
+        support service. Our team will shortly reach out to you regarding the
+        confirmation of your order.
+      </p>
+      <div className="flex gap-[20px] my-[18px]">
+        <div
+          onClick={gotoHome}
+          className="cursor-pointer py-2 px-3 rounded-xl border  hover:bg-black hover:border-black hover:text-white duration-300 active:bg-gray-300 active:text-black active:border-gray-300"
+        >
+          Home
+        </div>
+        <div
+          onClick={gotoPage}
+          className="cursor-pointer py-2 px-3 rounded-xl  border hover:bg-black hover:border-black hover:text-white duration-300 active:bg-gray-300 active:text-black active:border-gray-300"
+        >
+          Explore!
+        </div>
+      </div>
     </div>
   );
 };
