de14af8608f0a3781570cecdfd5880dcfdc47e6d
Muhammad Mubashir Munir Khan <100374421+mubashir05-beep@users.noreply.github.com>
2023-06-25 10:20:25 -0700

    Update HeroBanner.jsx

--- Front-End/khabi_store/components/HeroBanner.jsx
+++ Front-End/khabi_store/components/HeroBanner.jsx
@@ -2,34 +2,43 @@ import React from "react";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from "react-responsive-carousel";
 import { urlFor } from "@/lib/client";
+import Image from "next/image";
+Image
 const HeroBanner = ({ setBannerData }) => {
   return (
-    <div className="relative">
-      <Carousel
-        autoPlay={true}
-        infiniteLoop={true}
-        showThumbs={false}
-        showArrows={false}
-        interval={1000}
-        showStatus={false}
-        showIndicators={false}
-      >
-        {setBannerData?.map((banner) => (
-          <div key={banner._id}>
-            <img src={urlFor(banner.image)} alt="Slide 1" />
-          </div>
-        ))}
-      </Carousel>
-      {/* <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center">
-        <h1 className="text-4xl font-bold text-white">Welcome to our store</h1>
-        <p className="mt-4 text-lg text-white">
-          Shop our latest collection now!
-        </p>
-        <button className="mt-8 px-8 py-3 bg-white text-black rounded-full hover:bg-gray-300 transition-colors duration-300">
-          hello
-        </button>
-      </div> */}
+    <div className="display flex">
+      <div className="relative ">
+
+<Carousel
+  autoPlay={true}
+  infiniteLoop={true}
+  showThumbs={false}
+  showArrows={false}
+  interval={1000}
+  showStatus={false}
+  showIndicators={false}
+>
+  {setBannerData?.map((banner) => (
+    <div key={banner._id} className="w-[500px]">
+      <img src={urlFor(banner.image)} width={'200px'} alt="Slide" />
     </div>
+  ))}
+</Carousel>
+{/* <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center">
+  <h1 className="text-4xl font-bold text-white">Welcome to our store</h1>
+  <p className="mt-4 text-lg text-white">
+    Shop our latest collection now!
+  </p>
+  <button className="mt-8 px-8 py-3 bg-white text-black rounded-full hover:bg-gray-300 transition-colors duration-300">
+    hello
+  </button>
+</div> */}
+</div>
+<div  className="w-[50%]">
+  Hello
+</div>
+    </div>
+    
   );
 };
 
