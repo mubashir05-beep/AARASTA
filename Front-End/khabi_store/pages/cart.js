455c42a259ceb1261619bd55a06fd6943f8fe926
Muhammad Mubashir Munir Khan <100374421+mubashir05-beep@users.noreply.github.com>
2023-07-08 19:46:06 +0500

    Update cart.js

--- Front-End/khabi_store/pages/cart.js
+++ Front-End/khabi_store/pages/cart.js
@@ -452,105 +452,125 @@ const Cart = ({ coupons }) => {
               lock ? "opacity-50 pointer-events-none" : ""
             }`}
           >
-          {cartItems.length >= 1 &&
-  cartItems.map((items, index) => (
-    <div className="flex items-center py-5 border-b border-l" key={items._id}>
-      <div className="flex items-center gap-6 w-full">
-        <div>
-          <div className="relative">
-            <img
-              src={urlFor(items?.image[0])}
-              width={"200px"}
-              className="rounded-lg"
-            />
-            {items.discount && (
-              <span className="absolute top-0 right-0 z-10 bg-red-500 rounded-tr-lg text-white px-2 py-1 text-xs font-bold">
-                {((items.discount / items.price) * 100).toFixed(0)}% OFF
-              </span>
-            )}
-          </div>
-        </div>
+            {cartItems.length >= 1 &&
+              cartItems.map((items, index) => (
+                <div className="flex  border-b  border-l py-5 " key={items._id}>
+                  <div className="flex min-[500px]:flex-row md:flex-row items-center md:items-start flex-col gap-6 md:gap-[3rem] md:justify-evenly w-[100%] ">
+                    <div>
+                      <div className="relative">
+                        <img
+                          src={urlFor(items?.image[0])}
+                          width={"200px"}
+                          className="rounded-lg"
+                        />
+                        {items.discount && (
+                          <span className="absolute top-0 right-0 z-10 bg-red-500 rounded-tr-lg text-white px-2 py-1 text-xs font-bold">
+                            {((items.discount / items.price) * 100).toFixed(0)}%
+                            OFF
+                          </span>
+                        )}
+                      </div>
+                    </div>
 
-        <div className="flex flex-col gap-4 md:gap-6">
-          <div className="font-semibold text-lg break-words">
-            <Link href={`./ready_to_wear/${items.slug.current}`}>
-              {items.name}
-            </Link>
-          </div>
-          <div className="text-sm text-gray-500">
-            Code: {items.productCode}
-          </div>
-          <div className="text-sm text-gray-500">
-            <Link href={`./ready_to_wear/`}>{items.category}</Link>
-          </div>
-          <div className="text-sm flex gap-1 items-center py-1 text-gray-500">
-            Size:
-            <span className="w-6 h-6 text-xs text-white rounded-full bg-black/20 flex items-center justify-center">
-              {items.size}
-            </span>
-          </div>
-          <div className="hidden md:block">
-            {items.quantity ? (
-              <div className="text-green-400 text-sm">In Stock</div>
-            ) : (
-              <div className="text-red-400 text-sm">Out of Stock</div>
-            )}
-          </div>
-        </div>
-
-        <div className="flex flex-col gap-4 md:gap-6">
-          <div className="flex flex-col items-start">
-            <div className="font-medium text-base text-gray-600">
-              Per Price
-            </div>
-            {items.discount ? (
-              <div className="flex flex-col items-start">
-                <div className="text-xl text-gray-600">
-                  PKR {items.price - items.discount}
+                    <div className="flex flex-col md:flex-row items-center min-[500px]:items-start">
+                      <div className="flex flex-col gap-2 items-center min-[500px]:items-start md:my-6 w-[200px]">
+                        <div className="font-semibold text-lg break-words">
+                          <Link href={`./ready_to_wear/${items.slug.current}`}>
+                            {items.name}
+                          </Link>
+                        </div>
+                        <div className="text-[15px] text-gray-500">
+                          Code: {items.productCode}
+                        </div>
+                        <div className="text-[15px] text-gray-500">
+                          <Link href={`./ready_to_wear/`}>
+                            {items.category}
+                          </Link>
+                        </div>
+                        <div className="text-[15px] flex gap-1 items-center py-1 text-gray-500">
+                          {" "}
+                          Size:
+                          <span className="w-[20px] h-[20px] text-[12px] text-black rounded-full bg-black/[0.1] flex items-center justify-center">
+                            {items.size}
+                          </span>
+                        </div>
+                        <div className="hidden md:block">
+                          {items.quantity ? (
+                            <div className="text-green-400 text-[14px]">
+                              In Stock
+                            </div>
+                          ) : (
+                            <div className="text-red-400 text-[14px]">
+                              Out of Stock
+                            </div>
+                          )}
+                        </div>
+                      </div>
+                      <div className="flex flex-col  gap-2 md:w-[100px] md:items-center md:my-6">
+                        <div className="flex flex-col gap-2 md:my-1 items-center">
+                          <div className="font-[500] text-[15px] text-lg hidden md:block">
+                            Per Price
+                          </div>
+                          {items.discount ? (
+                            <div className="flex flex-col items-center justify-start  ">
+                              <div className="text-[18px] text-gray-600 items-center">
+                                PKR {items.price - items.discount}
+                              </div>
+                              <div className="text-[14px] text-gray-600 line-through">
+                                PKR {items.price}
+                              </div>
+                            </div>
+                          ) : (
+                            <div className="text-[16px] text-gray-600">
+                              PKR {items.price}
+                            </div>
+                          )}
+                        </div>
+                        <div className="flex flex-col md:my-1 gap-2  items-center ">
+                          <div className="font-[500] text-[15px] text-lg hidden md:block">
+                            Quantity
+                          </div>
+                          <div className="flex items-center ">
+                            <button
+                              className="px-2 bg-gray-200 text-gray-700 rounded-l"
+                              onClick={() =>
+                                toggleCartItemQuanitity(items._id, "dec")
+                              }
+                            >
+                              -
+                            </button>
+                            <span className="mx-2 text-lg text-gray-600">
+                              {items.quantity}
+                            </span>
+                            <button
+                              className="px-2 bg-gray-200 text-gray-700 rounded-r"
+                              onClick={() =>
+                                toggleCartItemQuanitity(items._id, "inc")
+                              }
+                            >
+                              +
+                            </button>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="w-[100px] md:block hidden">
+                      <div className="w-[100px] md:block hidden">
+                        <div className="w-[100px] md:block hidden">
+                          <RxCross2
+                            size={25}
+                            className={`cursor-pointer ${
+                              processing ? "opacity-50" : ""
+                            }`}
+                            disabled={lock}
+                            onClick={() => !processing && onRemove(items)}
+                          />
+                        </div>
+                      </div>
+                    </div>
+                  </div>
                 </div>
-                <div className="text-sm text-gray-400 line-through">
-                  PKR {items.price}
-                </div>
-              </div>
-            ) : (
-              <div className="text-xl text-gray-600">PKR {items.price}</div>
-            )}
-          </div>
-
-          <div className="flex items-center">
-            <div className="font-medium text-base text-gray-600">Quantity</div>
-            <div className="flex items-center ml-2">
-              <button
-                className="px-3 py-1 bg-gray-200 text-gray-700 rounded-l"
-                onClick={() => toggleCartItemQuanitity(items._id, "dec")}
-              >
-                -
-              </button>
-              <span className="px-3 py-1 text-lg text-gray-600">
-                {items.quantity}
-              </span>
-              <button
-                className="px-3 py-1 bg-gray-200 text-gray-700 rounded-r"
-                onClick={() => toggleCartItemQuanitity(items._id, "inc")}
-              >
-                +
-              </button>
-            </div>
-          </div>
-        </div>
-
-        <div className="hidden md:block">
-          <RxCross2
-            size={25}
-            className={`cursor-pointer ${processing ? "opacity-50" : ""}`}
-            disabled={lock}
-            onClick={() => !processing && onRemove(items)}
-          />
-        </div>
-      </div>
-    </div>
-  ))}
-
+              ))}
           </div>
           <div className="flex flex-[0.5] border-l border-r justify-center border-b">
             {cartItems.length >= 1 && (
