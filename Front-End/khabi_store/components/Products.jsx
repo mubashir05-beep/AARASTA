8637434a7c9d9dd7a7d29b97bdfb5ff0db1b395a
Muhammad Mubashir Munir Khan <100374421+mubashir05-beep@users.noreply.github.com>
2023-07-09 19:40:49 +0500

    added sorting

--- Front-End/khabi_store/components/Products.jsx
+++ Front-End/khabi_store/components/Products.jsx
@@ -3,21 +3,27 @@ import { urlFor } from "@/lib/client";
 import Link from "next/link";
 import { useStateContext } from "@/context/StateContext";
 
-const Products = ({ product }) => {
-  const { incQty, onAdd } = useStateContext();
+const ProductCard = ({ product }) => {
+  const { incQty } = useStateContext();
+
+  const discountPercentage = product.discount
+    ? ((product.discount / product.price) * 100).toFixed(0)
+    : null;
+
+  const discountedPrice = product.price - product.discount;
 
   return (
-    <div className="bg-white shadow-lg rounded-lg overflow-hidden">
+    <div className="bg-white rounded-lg overflow-hidden shadow-lg">
       <Link href={`/ready_to_wear/${product.slug.current}`}>
         <div className="relative cursor-pointer">
           <img
-            className="w-full h-32 object-cover"
+            className="w-full h-[400px] object-cover"
             src={urlFor(product.image && product.image[0])}
             alt={product.name}
           />
           {product.discount && (
             <span className="absolute top-2 right-2 z-10 bg-red-500 text-white py-1 px-2 text-xs font-bold rounded">
-              {((product.discount / product.price) * 100).toFixed(0)}% OFF
+              {discountPercentage}% OFF
             </span>
           )}
         </div>
@@ -33,25 +39,26 @@ const Products = ({ product }) => {
             {product.name}
           </h1>
         </Link>
-        <p className="text-gray-800 font-medium text-sm mb-4">PKR {product.price}</p>
+        {product.discount && (
+          <div className="flex items-center mb-2">
+            <p className="text-red-500 text-xl font-medium pr-2">
+              PKR {discountedPrice}
+            </p>
+            <p className="text-gray-500 text-sm line-through">
+              PKR {product.price}
+            </p>
+          </div>
+        )}
+        {!product.discount && (
+          <p className="text-gray-800 font-medium text-sm mb-4">
+            PKR {product.price}
+          </p>
+        )}
         <div className="flex items-center justify-center">
           <Link href={`/ready_to_wear/${product.slug.current}`}>
-            <button className="flex items-center px-4 py-2 bg-transparent text-black hover:underline focus:outline-none border border-black rounded">
-              View More
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-6 w-6 ml-2"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
-                />
-              </svg>
+            <button className="flex items-center px-4 py-2 bg-transparent text-black hover:border-gray-500 focus:outline-none border border-black rounded">
+              Learn More
+            
             </button>
           </Link>
         </div>
@@ -60,4 +67,4 @@ const Products = ({ product }) => {
   );
 };
 
-export default Products;
+export default ProductCard;
--- Front-End/khabi_store/pages/ready_to_wear.js
+++ Front-End/khabi_store/pages/ready_to_wear.js
@@ -1,21 +1,92 @@
-import React from "react";
+import React, { useState } from "react";
 import { client } from "@/lib/client";
 import Products from "@/components/Products";
 import Link from "next/link";
 
 const ReadyToWear = ({ products }) => {
+  const [sortBy, setSortBy] = useState("");
+
+  const handleSortChange = (event) => {
+    setSortBy(event.target.value);
+  };
+
+  const sortProducts = () => {
+    let sortedProducts = [...products];
+
+    if (sortBy === "priceLowToHigh") {
+      sortedProducts.sort((a, b) => {
+        const discountedPriceA = calculateDiscountedPrice(a);
+        const discountedPriceB = calculateDiscountedPrice(b);
+
+        if (discountedPriceA === discountedPriceB) {
+          return a.price - b.price; // Sort by actual price if discounted prices are equal
+        }
+
+        return discountedPriceA - discountedPriceB;
+      });
+    } else if (sortBy === "priceHighToLow") {
+      sortedProducts.sort((a, b) => {
+        const discountedPriceA = calculateDiscountedPrice(a);
+        const discountedPriceB = calculateDiscountedPrice(b);
+
+        if (discountedPriceA === discountedPriceB) {
+          return b.price - a.price; // Sort by actual price if discounted prices are equal
+        }
+
+        return discountedPriceB - discountedPriceA;
+      });
+    } else if (sortBy === "nameAtoZ") {
+      sortedProducts.sort((a, b) => a.name.localeCompare(b.name));
+    } else if (sortBy === "nameZtoA") {
+      sortedProducts.sort((a, b) => b.name.localeCompare(a.name));
+    }
+
+    return sortedProducts;
+  };
+
+  const calculateDiscountedPrice = (product) => {
+    if (product.discount) {
+      return product.price - product.discount;
+    }
+    return product.price;
+  };
+
+  const sortedProducts = sortProducts();
+
   return (
     <>
-      <div className="flex flex-col lg:flex-row mx-3 my-3 gap-6">
-        <Link href="/" className="font-medium hover:underline">
+      <div className="flex flex-row mx-[3rem] mt-[3rem] gap-2">
+        <Link href="/" className="hover:underline">
           Home
         </Link>
         <span>/</span>
-        <span className="font-medium">Shirts</span>
+        <span className="font-semibold">Shirts</span>
       </div>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 px-5 mx-3 my-5 md:px-0">
-        {products.map((product) => (
-          <div key={product._id}>
+      <div className="mb-4 mx-[3rem] mt-[1rem] flex items-center">
+  <span className="mr-[12px]">Sort by:</span>
+  <select
+    value={sortBy}
+    onChange={handleSortChange}
+    className="py-2  border border-gray-300 rounded"
+  >
+    <option value="">None</option>
+    <option value="priceLowToHigh">Price: Low to High</option>
+    <option value="priceHighToLow">Price: High to Low</option>
+    <option value="nameAtoZ">Name: A to Z</option>
+    <option value="nameZtoA">Name: Z to A</option>
+  </select>
+</div>
+
+      <div className="grid grid-cols-1 md:grid-cols-2 mx-[3rem] lg:grid-cols-3 gap-5 px-5 my-[4rem] md:px-0">
+        {sortedProducts.map((product) => (
+          <div
+            key={product._id}
+            style={{
+              border: "1px solid #ccc",
+              padding: "1rem",
+              borderRadius: "0.5rem",
+            }}
+          >
             <Products product={product} />
           </div>
         ))}
